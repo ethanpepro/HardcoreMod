57730ce68314f0bb0d56d14188164897d5c81047
James Luke <5251299+ethanpepro@users.noreply.github.com>
2021-12-13 10:57:11 -0500

    Rename the project to HardcoreMod and adjust project settings

---
+++ src/main/java/com/ethanpepro/hardcoremod/HardcoreMod.java
@@ -0,0 +1,14 @@
+package com.ethanpepro.hardcoremod;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HardcoreMod implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("hardcoremod");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
